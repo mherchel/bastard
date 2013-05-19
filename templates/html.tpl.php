c190f88d14fa9bd253ae7585d932053dc12fe542
unknown <fhsaa@lt-mherchel-w7.(none)>
2013-05-19 09:42:20 -0700

    Moving scripts to bottom of page.tpl.php.

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -6,11 +6,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no" />
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
-  <?php print $scripts; ?>
   <!--[if lte IE 9]>
   <link rel="stylesheet" href="/<?php print drupal_get_path('theme',$GLOBALS['theme']); ?>/css/ie-lte-9.css" media="all" />
   <![endif]-->
-
   <!--[if lte IE 8]>
   <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <link rel="stylesheet" href="/<?php print drupal_get_path('theme',$GLOBALS['theme']); ?>/css/narrow.css" media="all" />
@@ -29,5 +27,6 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+  <?php print $scripts; ?>
 </body>
 </html>
